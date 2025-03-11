bf26b930e1c35de8540ec205430d0038f8afcab6
Jacuzzi <jheklayag@gmail.com>
2025-03-11 13:46:24 +0800

    User input for two numbers have been added

--- print_the_product_of_two_numbers.py
+++ print_the_product_of_two_numbers.py
@@ -1,4 +1,8 @@
 # Ask user to input first number
+first_num = int(input("Enter a number: "))
+
 # Ask user to input second number
+second_num = int(input("Enter a number: "))
+
 # Multiply the two numbers
 # Print result
