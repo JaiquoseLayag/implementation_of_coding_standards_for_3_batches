ef159895b706147813510e24213eef18703b573b
Jacuzzi <jheklayag@gmail.com>
2025-03-11 14:45:50 +0800

    User input for 10 numbers have been added

--- print_the_count_of_odd_numbers.py
+++ print_the_count_of_odd_numbers.py
@@ -2,6 +2,9 @@
 count = 0
 
 # Use a loop to ask user to input 10 numbers
+for i in range(10):
+    i = int(input("Enter a number: "))
+    
 # Check each number if they are odd
 # Add 1 to the count for each odd number found
 # Print the total count
