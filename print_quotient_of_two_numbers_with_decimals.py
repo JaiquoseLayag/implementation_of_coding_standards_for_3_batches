a2b6bad5ef1935a1085c229702be840e0cbe6548
Jacuzzi <jheklayag@gmail.com>
2025-03-11 13:59:21 +0800

    Arithmetic operator (/) has been added

--- print_quotient_of_two_numbers_with_decimals.py
+++ print_quotient_of_two_numbers_with_decimals.py
@@ -3,5 +3,8 @@ first_num = int(input("Enter a number: "))
 
 # Ask the user to input second number
 second_num = int(input("Enter a number: "))
+
 # Divide the two numbers
+quotient = first_num / second_num
+
 # Print the quotient
