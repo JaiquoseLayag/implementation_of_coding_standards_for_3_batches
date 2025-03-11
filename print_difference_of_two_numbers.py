9bdebba33fe3d355f807c831cfffd825e4fed38c
Jacuzzi <jheklayag@gmail.com>
2025-03-11 21:45:47 +0800

    User input for two numbers have been added

--- print_difference_of_two_numbers.py
+++ print_difference_of_two_numbers.py
@@ -1,4 +1,8 @@
 # Ask user to input the first number
+first_num = int(input("Enter a number: "))
+
 # Ask user to input the second number
+second_num = int(input("Enter a number: "))
+
 # Subtract the second number from the first number
 # Print difference
