5c29c742ab379687c01364e1a5c84c10a50fff33
Jacuzzi <jheklayag@gmail.com>
2025-03-11 21:36:07 +0800

    User input for two numbers have been added

--- print_Not_Equal_if_numbers_not_same.py
+++ print_Not_Equal_if_numbers_not_same.py
@@ -1,4 +1,8 @@
 # Ask the user to input the first number
+first_num = int(input("Enter a number: "))
+
 # Ask the user to input the second number
+second_num = int(input("Enter a number: "))
+
 # Check to see if they are not the same numbers
 # Print result
