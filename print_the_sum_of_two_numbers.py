41391291f2ccf983a51cde6dee9e00b0d14d9919
Jacuzzi <jheklayag@gmail.com>
2025-03-11 12:52:30 +0800

    Logical operator (+) for two numbers has been added

--- print_the_sum_of_two_numbers.py
+++ print_the_sum_of_two_numbers.py
@@ -5,4 +5,6 @@ first_num = int(input("Enter a number: "))
 second_num = int(input("Enter a number: "))
 
 # Find the sum of the two numbers
+sum_of_two_numbers = first_num + second_num
+
 # Print the result
