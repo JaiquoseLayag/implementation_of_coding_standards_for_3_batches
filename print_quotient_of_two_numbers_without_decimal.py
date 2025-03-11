3ef2847f3a964f6d9f1769baac5f058f0c139973
Jacuzzi <jheklayag@gmail.com>
2025-03-11 21:52:05 +0800

    User input for two numbers have been added

--- print_quotient_of_two_numbers_without_decimal.py
+++ print_quotient_of_two_numbers_without_decimal.py
@@ -1,4 +1,8 @@
 # Ask the user to input the first number
+first_num = int(input("Enter a number: "))
+
 # Ask the user to input the second number
+second_num = int(input("Enter a number: "))
+
 # Use floor division (//) to divide two numbers
 # Print quotient without decimals
