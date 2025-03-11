995f3848a2b43fa47dae8b0882b225fae6356800
Jacuzzi <jheklayag@gmail.com>
2025-03-11 14:05:26 +0800

    User input for two numbers have been added

--- result_when_first_number_raised_by_second_number.py
+++ result_when_first_number_raised_by_second_number.py
@@ -1,4 +1,8 @@
 # Ask user to input first number
+first_num = int(input("Enter a number: "))
+
 # Ask user to input second number
+second_num = int(input("Enter a number: "))
+
 # Raise first number by the second number
 # Print result
