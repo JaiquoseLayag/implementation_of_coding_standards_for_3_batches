df9307be2cfde016aa9fd325c67d2c10b82c1454
Jacuzzi <jheklayag@gmail.com>
2025-03-11 14:31:53 +0800

    Every consecutive user input is added to the overall total

--- print_the_sum_of_10_numbers.py
+++ print_the_sum_of_10_numbers.py
@@ -3,4 +3,8 @@ total = 0
 
 # Use a loop and ask the user to input 10 numbers
 # Add the previous input to the total
+for i in range(10):
+    i = int(input("Enter a number: "))
+    total = total + i
+    
 # Print total
