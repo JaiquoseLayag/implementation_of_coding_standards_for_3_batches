5f2ae626bc8bec3d23f6bf7c41a388ff07ec8e9d
Jacuzzi <jheklayag@gmail.com>
2025-03-11 13:58:16 +0800

    User input for two numbers have been added

--- print_quotient_of_two_numbers_with_decimals.py
+++ print_quotient_of_two_numbers_with_decimals.py
@@ -1,4 +1,7 @@
 # Ask the user to input first number
+first_num = int(input("Enter a number: "))
+
 # Ask the user to input second number
+second_num = int(input("Enter a number: "))
 # Divide the two numbers
 # Print the quotient
