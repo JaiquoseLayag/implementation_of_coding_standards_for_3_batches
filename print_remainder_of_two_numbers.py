375e86920f9e6f1245b5ce8c640209c1f6f4dece
Jacuzzi <jheklayag@gmail.com>
2025-03-11 22:01:08 +0800

    User input for two numbers have been added

--- print_remainder_of_two_numbers.py
+++ print_remainder_of_two_numbers.py
@@ -1,4 +1,8 @@
 # Ask the user to input first number
+first_num = int(input("Enter a number: "))
+
 # Ask the user to input second number
+second_num = int(input("Enter a number: "))
+
 # Use modulus (%) to find the remainder
 # Print remainder
