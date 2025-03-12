8fd73f4d7ffef288bc039e0386fbe61dde9da1f4
Jacuzzi <jheklayag@gmail.com>
2025-03-12 16:01:00 +0800

    User input for numbers have been added

--- batch_3_print_numbers_from_lowest_to_highest.py
+++ batch_3_print_numbers_from_lowest_to_highest.py
@@ -1,3 +1,10 @@
 # Use a list and ask user to input numbers while in a loop
+entered_numbers = []  
+
+while True:
+    try:
+        numbers = input("Enter a number: ")  
+        num = int(numbers)    
+        entered_numbers.append(num)    
 # Break the loop once input is invalid
 # Sort the list from lowest to highest and print the list
