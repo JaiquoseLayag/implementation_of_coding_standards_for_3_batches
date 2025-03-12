493f9a7d517497a3c1824c1c9fb186c05b416aa7
Jacuzzi <jheklayag@gmail.com>
2025-03-12 14:10:00 +0800

    Moved all 10 files into a folder

---
+++ batch_2_python_program_with_coding_standard/print_Not_Equal_if_numbers_not_same.py
@@ -0,0 +1,10 @@
+# Ask the user to input the first number
+first_num = int(input("Enter a number: "))
+
+# Ask the user to input the second number
+second_num = int(input("Enter a number: "))
+
+# Check to see if they are not the same numbers
+# Print result
+if first_num != second_num:
+    print("Not Equal")
---
+++ batch_2_python_program_with_coding_standard/result_of_first_number_minus_remaining_numbers.py
@@ -0,0 +1,13 @@
+# Use a list to store all inputs
+numbers = []
+# Use a loop and ask the user to input 10 numbers
+for i in range(10):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
+# Set the first number and add the remaining numbers 
+    first_num = numbers[0]
+    remaining_num = numbers[1:]
+# Subtract the sum of the remaining numbers from the first number
+    result - first_num - remaining_num
+# Print result
+print(result)
