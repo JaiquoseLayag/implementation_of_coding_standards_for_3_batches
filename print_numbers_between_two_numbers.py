bc55d942a8f33706a45e515d78514531db956ffb
Jacuzzi <jheklayag@gmail.com>
2025-03-12 13:58:48 +0800

    user input for two numbers have been added

--- print_numbers_between_two_numbers.py
+++ print_numbers_between_two_numbers.py
@@ -1,5 +1,7 @@
 # Ask the user to input the first number
+first_num = int(input("Enter a number: "))
 # Ask the user to input the second number
+second_num = int(input("Enter a number: "))
 # Apply a condition to let the smaller number be the first number
 # Use a loop and increment by 1 until it reaches the bigger number
 # Print the number
