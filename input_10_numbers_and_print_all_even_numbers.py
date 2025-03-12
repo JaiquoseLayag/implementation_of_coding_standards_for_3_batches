3f4e3b5e84691821213d0ee4d5a2d0a9180d746b
Jacuzzi <jheklayag@gmail.com>
2025-03-12 13:36:30 +0800

    Fixed a mistake, added a count to display the numebr of even numbers inputted by the user

--- input_10_numbers_and_print_all_even_numbers.py
+++ input_10_numbers_and_print_all_even_numbers.py
@@ -1,7 +1,9 @@
+count = 0
 # Use a loop and ask the user to input 10 numbers
 for i in range(10):
     num = int(input("Enter a number: "))
 # Use modulus (%) to check if numbers are even
     if num % 2 == 0:
+        count += 1
 # Print all even numbers
-print(num)
+print(count)
