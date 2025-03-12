8b797512ebba9e4a73a4ac3159fbc0af823628e0
Jacuzzi <jheklayag@gmail.com>
2025-03-12 14:57:34 +0800

    Side note: Pseudocode was the first commit; User input have been added and appended to the list

--- batch_3_print_numbers_with_the_first_entry_only.py
+++ batch_3_print_numbers_with_the_first_entry_only.py
@@ -1,3 +1,10 @@
 # Make a list and loop to ask the user to input 10 numbers and append to the list
+numbers = []  
+first_entry = set()  
+
+
+for i in range(10):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
 # Use a loop and use a set to ensure the first entry is printed
 # Print the numbers
