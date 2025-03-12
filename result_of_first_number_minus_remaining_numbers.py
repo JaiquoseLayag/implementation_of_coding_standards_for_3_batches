d41cf7003127a85abe7dd23e5c9b71298973b0c4
Jacuzzi <jheklayag@gmail.com>
2025-03-12 13:17:57 +0800

    User input for 10 numbers have been added

--- result_of_first_number_minus_remaining_numbers.py
+++ result_of_first_number_minus_remaining_numbers.py
@@ -1,5 +1,8 @@
 # Use a list to store all inputs
+numbers = []
 # Use a loop and ask the user to input 10 numbers
+for i in range(10):
+    num = int(input("Enter a number: "))
 # Set the first number and add the remaining numbers 
 # Subtract the sum of the remaining numbers from the first number
 # Print result
