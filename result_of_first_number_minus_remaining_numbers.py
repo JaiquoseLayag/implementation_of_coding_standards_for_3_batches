63190802bc5a4ed4d500b78619171651cd4336f7
Jacuzzi <jheklayag@gmail.com>
2025-03-12 13:20:01 +0800

    Remaining numbers have been added together

--- result_of_first_number_minus_remaining_numbers.py
+++ result_of_first_number_minus_remaining_numbers.py
@@ -4,5 +4,7 @@ numbers = []
 for i in range(10):
     num = int(input("Enter a number: "))
 # Set the first number and add the remaining numbers 
+    first_num = numbers[0]
+    remaining_num = numbers[1:]
 # Subtract the sum of the remaining numbers from the first number
 # Print result
