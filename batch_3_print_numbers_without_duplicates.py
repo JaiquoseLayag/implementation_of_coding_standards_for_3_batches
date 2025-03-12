89f9c86a8dd0c21fea62b87a242afc62ffd94e76
Jacuzzi <jheklayag@gmail.com>
2025-03-12 14:37:55 +0800

    User input for 10 numbers have been added and appended to the list

--- batch_3_print_numbers_without_duplicates.py
+++ batch_3_print_numbers_without_duplicates.py
@@ -1,3 +1,8 @@
 # Ask to user to input 10 numbers and append it to the list
+numbers = []
+
+for i in range(10):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
 # Make another list and count each item in the list
 # Print all numbers with a count of 1
