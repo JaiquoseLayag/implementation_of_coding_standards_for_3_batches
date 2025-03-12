1f30fa3964027c44dd3ea78db79d8af9550bb2d1
Jacuzzi <jheklayag@gmail.com>
2025-03-12 15:35:15 +0800

    User input have been added

--- batch_3_print_Unique_or_Duplicate_after_input.py
+++ batch_3_print_Unique_or_Duplicate_after_input.py
@@ -1,3 +1,9 @@
 # Use a set and ask the user to input numbers while in a loop
+entered_numbers = set()
+
+while True:
+    try:
+        numbers = input("Enter a number: ")
+        int(numbers)
 # Store numbers in the set and print along with Unique if it's not in the set and Duplicate if it's in the set
 # Break the loop once the input is invalid
