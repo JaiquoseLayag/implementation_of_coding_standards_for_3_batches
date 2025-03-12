5392e6a53f201c93dbbce83834b054038ed8ffc9
Jacuzzi <jheklayag@gmail.com>
2025-03-12 15:48:43 +0800

    Pseudocode

---
+++ batch_3_print_the_smallest_number.py
@@ -0,0 +1,3 @@
+# Use a set and ask the user to input numbers while in a loop
+# Store the numbers in a set and check which is the smallest
+# Break the loop once the input is invalid and print the smallest number
