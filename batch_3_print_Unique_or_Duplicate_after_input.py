e0fa23abc51ece4cdaccb0a7c49ad463b14c4aa3
Jacuzzi <jheklayag@gmail.com>
2025-03-12 15:37:45 +0800

    Conditions for Unique or Duplicate have been added

--- batch_3_print_Unique_or_Duplicate_after_input.py
+++ batch_3_print_Unique_or_Duplicate_after_input.py
@@ -6,4 +6,9 @@ while True:
         numbers = input("Enter a number: ")
         int(numbers)
 # Store numbers in the set and print along with Unique if it's not in the set and Duplicate if it's in the set
+        if numbers in entered_numbers:
+            print("Duplicate")
+        else:
+            print("Unique")
+            entered_numbers.add(numbers)
 # Break the loop once the input is invalid
