74bbc873db75e24b203cd4cab168d5869135d848
Jacuzzi <jheklayag@gmail.com>
2025-03-12 15:32:55 +0800

    Pseduocode

---
+++ batch_3_print_Unique_or_Duplicate_after_input.py
@@ -0,0 +1,3 @@
+# Use a set and ask the user to input numbers while in a loop
+# Store numbers in the set and print along with Unique if it's not in the set and Duplicate if it's in the set
+# Break the loop once the input is invalid
