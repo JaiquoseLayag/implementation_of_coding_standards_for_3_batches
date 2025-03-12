3b5c0d3e74196701d4106bf1af26e69acb37229b
Jacuzzi <jheklayag@gmail.com>
2025-03-12 14:59:03 +0800

    Condition to ensure first entry of numbers have been met

--- batch_3_print_numbers_with_the_first_entry_only.py
+++ batch_3_print_numbers_with_the_first_entry_only.py
@@ -7,4 +7,6 @@ for i in range(10):
     num = int(input("Enter a number: "))
     numbers.append(num)
 # Use a loop and use a set to ensure the first entry is printed
+for num in numbers:
+    if num not in first_entry:
 # Print the numbers
