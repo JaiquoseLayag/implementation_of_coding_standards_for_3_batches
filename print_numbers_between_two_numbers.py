08c3e60959bd284b6a88f9bd6c892288f3d18a20
Jacuzzi <jheklayag@gmail.com>
2025-03-12 13:57:19 +0800

    Pseudocode

---
+++ print_numbers_between_two_numbers.py
@@ -0,0 +1,5 @@
+# Ask the user to input the first number
+# Ask the user to input the second number
+# Apply a condition to let the smaller number be the first number
+# Use a loop and increment by 1 until it reaches the bigger number
+# Print the number
