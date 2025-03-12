55289f918d957d63dcb8a9dd4e981144c41f85a2
Jacuzzi <jheklayag@gmail.com>
2025-03-12 14:39:43 +0800

    Count each item in the list and append all items with 1 count only to the new list

--- batch_3_print_numbers_without_duplicates.py
+++ batch_3_print_numbers_without_duplicates.py
@@ -5,4 +5,9 @@ for i in range(10):
     num = int(input("Enter a number: "))
     numbers.append(num)
 # Make another list and count each item in the list
+non_duplicates = []
+
+for num in numbers:
+    if numbers.count(num) == 1:
+        non_duplicate.append(num)
 # Print all numbers with a count of 1
