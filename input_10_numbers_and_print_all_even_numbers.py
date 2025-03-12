c8e10806497b19f54aee407dc41a535975ca2879
Jacuzzi <jheklayag@gmail.com>
2025-03-12 13:28:13 +0800

    User input for 10 numbers have been added

--- input_10_numbers_and_print_all_even_numbers.py
+++ input_10_numbers_and_print_all_even_numbers.py
@@ -1,3 +1,5 @@
 # Use a loop and ask the user to input 10 numbers
+for i in range(10):
+    num = int(input("Enter a number: "))
 # Use modulus (%) to check if numbers are even
 # Print all even numbers
