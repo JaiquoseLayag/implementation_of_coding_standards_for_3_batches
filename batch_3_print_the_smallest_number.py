01b7ebe20569841ac188a94cb497969002b4a39b
Jacuzzi <jheklayag@gmail.com>
2025-03-12 15:51:18 +0800

    User input for numbers have been added

--- batch_3_print_the_smallest_number.py
+++ batch_3_print_the_smallest_number.py
@@ -1,3 +1,10 @@
 # Use a set and ask the user to input numbers while in a loop
+entered_numbers = set()
+
+while True:
+    try:
+        numbers = input("Enter a number: ")
+        num = int(numbers)
+        entered_numbeers.add(num)                
 # Store the numbers in a set and check which is the smallest
 # Break the loop once the input is invalid and print the smallest number
