8255a7ace73646ddb5e5d3213bc9652a028c219d
Jacuzzi <jheklayag@gmail.com>
2025-03-12 15:53:34 +0800

    Loop breaks if input is invalid and prints the smallest number

--- batch_3_print_the_smallest_number.py
+++ batch_3_print_the_smallest_number.py
@@ -5,6 +5,12 @@ while True:
     try:
         numbers = input("Enter a number: ")
         num = int(numbers)
-        entered_numbeers.add(num)                
+        entered_numbers.add(num)                
 # Store the numbers in a set and check which is the smallest
 # Break the loop once the input is invalid and print the smallest number
+    except ValueError:
+        print("Invalid input. Exiting")
+        break
+
+if entered_numbers:
+    print("Smallest Number: " + str(main(entered_numbers)))
