870531005c66f7ed0c86e5e8a1b4f62b1ebade02
Jacuzzi <jheklayag@gmail.com>
2025-03-12 13:15:11 +0800

    Pseudocode

---
+++ result_of_first_number_minus_remaining_numbers.py
@@ -0,0 +1,5 @@
+# Use a list to store all inputs
+# Use a loop and ask the user to input 10 numbers
+# Set the first number and add the remaining numbers 
+# Subtract the sum of the remaining numbers from the first number
+# Print result
